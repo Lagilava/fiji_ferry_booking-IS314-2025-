8e7c038052e7417c8c769a3dcf2f9733ffcc20af
Lagilava <paulolagilava@gmail.com>
2026-06-28 22:21:28 +1200

    Fix on superuser not created to test Admin - modified build.sh

--- build.sh
+++ build.sh
@@ -6,3 +6,22 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+echo "Checking/creating superuser..."
+
+python manage.py shell << EOF
+import os
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+
+if username and not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+    print("Superuser created")
+else:
+    print("Superuser already exists or env vars missing")
+EOF
